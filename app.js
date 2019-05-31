1f63bf2b2d0751780a7fdd90850dbba0e578e5ec
IDM.air <innanadm8@gmail.com>
2019-05-30 20:52:58 -0400

    adding phrases to array

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("ready!");
+
+msg = new Array ("The Earth is the third planet from the Sun.", "It rains diamonds on Neptune and Uranus.", "Venus is the brightest planet in our Solar System.", "Pluto's existence was not known to the ancient world.")
