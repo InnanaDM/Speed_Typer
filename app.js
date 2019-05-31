1da4038f18642d88b7d5383d22c815998849936c
IDM.air <innanadm8@gmail.com>
2019-05-30 23:27:48 -0400

    rookie phrases added, s funct

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ msg = new Array ("The Earth is the third planet from the Sun.", "It rains diamon
 word = 10;
 
 function s() {
-    msg = new Array("The Sumerians were the first known beer drinking societies.", "Ninkasi was the Sumerian goddess of beer.", "Inanna is the Sumerian goddess of Heaven and Earth.")
+    msg = new Array("Ancient Sumer is the first civilization recorded in history.", "The Sumerians were the first known beer drinking society.", "Ninkasi is the Sumerian goddess of beer.", "Inanna is the Sumerian goddess of Heaven and Earth.", "Sumerian aliens were called the Annunaki.", "The Annunaki came to Earth to mine gold.", "Sumer is also known as 'land of the civilized kings'." );
+    word = 10; 
 }
 
 // "'I am' is the shortest complete sentence in the English language
